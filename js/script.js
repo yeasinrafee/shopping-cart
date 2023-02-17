d827c20c6d7e37001d1731bad4e7adecb53bcbce
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-02-17 22:09:16 +0600

    tax and total function addede

--- js/script.js
+++ js/script.js
@@ -16,6 +16,25 @@ const setSubTotal = function(){
     document.getElementById('sub-total').innerText = phoneTotal + caseTotal;
 }
 
+const setTax = function(){
+    const phoneTotal = getElementTextById('phone-total');
+    const caseTotal = getElementTextById('case-total');
+    const phoneCaseTotal = phoneTotal + caseTotal;
+    const tax = (phoneCaseTotal * 0.1).toFixed(2);
+
+    document.getElementById('tax').innerText = tax;
+}
+
+const setTotal = function(){
+    const phoneTotal = getElementTextById('phone-total');
+    const caseTotal = getElementTextById('case-total');
+    const phoneCaseTotal = phoneTotal + caseTotal;
+    const tax = (phoneCaseTotal * 0.1).toFixed(2);
+    const total = parseFloat(phoneCaseTotal) + parseFloat(tax);
+
+    document.getElementById('total').innerText = total;
+}
+
 document.getElementById('first-plus').addEventListener('click', function(){
     const increaseValue = getValueInput('first-input');
     document.getElementById('first-input').value = increaseValue + 1;
@@ -23,6 +42,8 @@ document.getElementById('first-plus').addEventListener('click', function(){
     document.getElementById('phone-total').innerText = phoneTotalPrice;
 
     setSubTotal();
+    setTax();
+    setTotal();
 })
 
 document.getElementById('first-minus').addEventListener('click', function(){
@@ -34,6 +55,8 @@ document.getElementById('first-minus').addEventListener('click', function(){
     document.getElementById('phone-total').innerText = phoneTotalPrice;
 
     setSubTotal();
+    setTax();
+    setTotal();
 })
 
 document.getElementById('second-plus').addEventListener('click', function(){
@@ -43,6 +66,8 @@ document.getElementById('second-plus').addEventListener('click', function(){
     document.getElementById('case-total').innerText = caseTotalPrice;
 
     setSubTotal();
+    setTax();
+    setTotal();
 })
 
 document.getElementById('second-minus').addEventListener('click', function(){
@@ -54,4 +79,6 @@ document.getElementById('second-minus').addEventListener('click', function(){
     document.getElementById('case-total').innerText = caseTotalPrice;
 
     setSubTotal();
+    setTax();
+    setTotal();
 })
